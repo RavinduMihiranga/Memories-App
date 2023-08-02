6f2bdc14de58eb8ea775f189d269b0fd108f8a89
RavinduMihiranga <e18224@eng.pdn.ac.lk>
2023-08-03 00:14:05 +0530

    connected the databse

--- server/index.js
+++ server/index.js
@@ -3,7 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+// initializing app for any express application
+const app = express();
 
+//  to handle request seting up a image upload size limit
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://ravindumihiranga788:QiD09c9XrbJeNdgm@cluster0.vota6mf.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
