cdc23a8230df9a5a0b4d61cac0f8a11d54bef635
RavinduMihiranga <e18224@eng.pdn.ac.lk>
2023-08-05 12:12:33 +0530

    finally client side worked

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {Container, AppBar, Typography} from '@mui/material';
 
-function App() {
+import memories from './images/memories.png'
+const App = () => {
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
-  );
+    <Container maxWidth ='lg'>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="memories" height="500"/>
+      </AppBar>
+
+    </Container>
+  )
+
 }
 
 export default App;
